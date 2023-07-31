4cffa98802f4771c5ffa24fdc29b8e826ff190d6
David_Diaz <htq3wj@virginia.edu>
2023-07-31 11:18:43 -0400

    Adding files

---
+++ movie_package/movie.py
@@ -0,0 +1,3 @@
+def best_movie():
+    '''This function prints the best movie of all time'''
+    print("The best movie of all time is Everything Everywhere All At Once")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='movie_package',
+    version='1.0.0',
+    url='https://github.com/daviddj2022/DS5100_M08_Challenge',
+    license='GNU General Public License v3.0',
+    author='David Diaz',
+    author_email='htq3wj@virginia.edu',
+    description='Ranks movies',
+    packages=find_packages()
+)
